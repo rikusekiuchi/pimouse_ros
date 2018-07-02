da7c9be0d5d78caff4f7209e8067ce9cc356327a
Riku Sekiuchi <s16c1751xd@s.chibakoudai.jp>
2018-07-02 06:48:16 +0000

    test

--- scripts/travis_test_buzzer.py
+++ scripts/travis_test_buzzer.py
@@ -13,8 +13,8 @@ class BuzzerTest(unittest.TestCase):
     def test_put_value(self):
         pub = rospy.publisher('/buzzer', UInt16)
         for i in range(10):
-        pub.publish(1234)
-        time.sleep(0.1)
+         pub.publish(1234)
+         time.sleep(0.1)
 
         with open("/dev/rtbuzzer0","r") as f:
         data = f.readline()
